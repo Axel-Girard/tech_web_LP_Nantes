d7c1859a7f34b998a749f48f71467500f651bed4
Axel Girard <axel.girard@etu.univ-nantes.fr>
2015-11-12 17:51:03 +0100

    ajouts

--- projet/scripts/recuperation.js
+++ projet/scripts/recuperation.js
@@ -1,13 +1,15 @@
-var yourUrl = "https://www.reddit.com/r/pics/.json";
+var yourUrl = "https://www.reddit.com/r/pics/.json?limit=50";
 var Httpreq = new XMLHttpRequest();
 Httpreq.open("GET", yourUrl, false);
 Httpreq.send(null);
 var data = Httpreq.responseText;
 
 obj = JSON.parse(data);
-for (cptImage = 0; cptImage < 20 ; cptImage++) {
-  document.getElementById("api").innerHTML += "<td><a href=\"" +
-    obj.data.children[cptImage].data.url + "\"><img src=" +
-    obj.data.children[cptImage].data.preview.images[0].source.url +
-    " alt=\"" + obj.data.children[cptImage].data.title + "\" /></a></td>";
+for (cptImage = 0; cptImage < 50 ; cptImage++) {
+	if(obj.data.children[cptImage].data.preview !== undefined){
+		document.getElementById("api").innerHTML += "<td><a href=\"" +
+			obj.data.children[cptImage].data.url + "\"><img src=" +
+			obj.data.children[cptImage].data.preview.images[0].source.url +
+			" alt=\"" + obj.data.children[cptImage].data.title + "\" /></a></td>";
+	}
 }
